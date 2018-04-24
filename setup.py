6edeff33f51d65653566ae4ba9433303cdfeba5a
patarapolw <patarapolw@gmail.com>
2018-04-24 22:12:53 +0700

    Update WordTool

--- setup.py
+++ setup.py
@@ -21,29 +21,6 @@ setup(
     url='https://github.com/patarapolw/randomsentence',  # Optional
     author='Pacharapol Withayasakpunt',  # Optional
     author_email='patarapolw@gmail.com',  # Optional
-    # classifiers=[  # Optional
-    #     # How mature is this project? Common values are
-    #     #   3 - Alpha
-    #     #   4 - Beta
-    #     #   5 - Production/Stable
-    #     'Development Status :: 3 - Alpha',
-    #
-    #     # Indicate who your project is intended for
-    #     'Intended Audience :: Developers',
-    #     'Topic :: Software Development :: Build Tools',
-    #
-    #     # Pick your license as you wish
-    #     'License :: OSI Approved :: MIT License',
-    #
-    #     # Specify the Python versions you support here. In particular, ensure
-    #     # that you indicate whether you support Python 2, Python 3 or both.
-    #     'Programming Language :: Python :: 2',
-    #     'Programming Language :: Python :: 2.7',
-    #     'Programming Language :: Python :: 3',
-    #     'Programming Language :: Python :: 3.4',
-    #     'Programming Language :: Python :: 3.5',
-    #     'Programming Language :: Python :: 3.6',
-    # ],
     keywords='random sentence random_sentence random_word',  # Optional
     packages=find_packages(exclude=['tests']),  # Required
     install_requires=install_requires,  # Optional
