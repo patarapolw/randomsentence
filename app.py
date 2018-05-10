be4300e2e3fe4ab299268b05b0fe64de4f0aeb82
patarapolw <patarapolw@gmail.com>
2018-05-10 16:28:49 +0700

    Wrap if __name__ == '__main__'

--- app.py
+++ app.py
@@ -2,7 +2,8 @@ import os
 
 from webdemo import randomsentenceapp
 
-if 'DYNO' in os.environ:
-    randomsentenceapp.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
-else:
-    randomsentenceapp.run(host='localhost', port=8080, debug=True)
+if __name__ == '__main__':
+    if 'DYNO' in os.environ:
+        randomsentenceapp.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+    else:
+        randomsentenceapp.run(host='localhost', port=8080, debug=True)
