564000d8c435ed07a778830242448735b30ee6b8
patarapolw <patarapolw@gmail.com>
2018-05-10 15:43:42 +0700

    first commit to Heroku

--- webdemo/__init__.py
+++ webdemo/__init__.py
@@ -1,4 +1,5 @@
-from bottle import Bottle, TEMPLATE_PATH, template, request
+from bottle import Bottle, TEMPLATE_PATH, template, request, static_file
+from diceware_utils.wordlist import Wordlist
 
 from randomsentence.sentence_maker import SentenceMaker
 # from randomsentence.randomsentence import RandomSentence
@@ -8,6 +9,7 @@ sentence_maker = SentenceMaker()
 # random_sentence = RandomSentence()
 random_sentence = sentence_maker.random_sentences
 sentence_tools = SentenceTools()
+word_list = Wordlist()
 
 TEMPLATE_PATH.append('./webdemo/templates/')
 randomsentenceapp = Bottle()
@@ -25,6 +27,17 @@ def generate():
         return sentence_tools.detokenize_tagged(random_sentence.get_tagged_sent())
     else:
         return render_token(keyword_list)
+
+
+@randomsentenceapp.post('/randomize_words')
+def randomize_words():
+    number_of_words = 6
+    return ' '.join([word_list.get_random_word() for _ in range(number_of_words)])
+
+
+@randomsentenceapp.get('/static/js/<filename:path>')
+def send_js(filename):
+    return static_file(filename, root='./webdemo/static/js')
 
 
 def render_token(keyword_list):
---
+++ webdemo/static/js/main.js
@@ -0,0 +1,14 @@
+$(function(){
+    $("#do_randomize_words").click(function(event) {
+        $.post('/randomize_words', function(data, textStatus, xhr) {
+            $("#keywords").val(data);
+        });
+    });
+
+    $("#do_generate_sentence").click(function(event) {
+        event.preventDefault();
+        $.post('/generate', $("#generated_sentence_form").serialize(), function(data, textStatus, xhr) {
+            $("#generated_sentence").html(data);
+        });
+    });
+});
